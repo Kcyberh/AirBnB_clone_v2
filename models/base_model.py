0abbe709f29e28320e99c18ab169ef4c365282e0
Joe-Chege <njorogejosephchege@gmail.com>
2023-08-08 14:15:14 +0300

    This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,14 +4,23 @@ from datetime import datetime
 import models
 import uuid
 
-Class BaseModel():
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        """ Class Base Model, Base model for the AirBnB Clone"""
-         if kwargs is None or len(kwargs) == 0:
+        """Initialize BaseModel attributes"""
+
+        if kwargs is None or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        
-         else:
-             
-             
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
+                elif key != "__class__":
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """ Class method to display in human readable BaseModel instance """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
