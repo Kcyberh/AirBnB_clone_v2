d304deb7791ca39340158d1343cba43c62b13cf1
Joe-chege <njorogejosephchege@gmail.com>
2023-08-13 14:10:06 -0700

    making coreection

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""
+Initialization script for the models directory.
+"""
+
 from models.engine.file_storage import FileStorage
 
+# Create an instance of FileStorage
 storage = FileStorage()
+
+# Load data from the JSON file into the storage instance
 storage.reload()
